cb945edcd83183e9800c79d5f573734409363d93
MAKWANA PARTH ASHOKBHAI <84260254+mkwparth@users.noreply.github.com>
2022-12-05 21:39:59 +0530

    vid 44 completed

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,9 @@ ConnectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Parth Makwana')
-})
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))  
+ 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+import mongoose, { models } from 'mongoose';
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+
+});
+
+module.exports=mongoose.model('user',UserSchema );
